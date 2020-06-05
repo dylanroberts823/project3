2abd19bbc5ae2c84a2335291259a02296e440ea8
dylanroberts823 <jguidote@hotmail.com>
2020-06-05 13:36:34 -0400

    Updated index to display items within categories

--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Categories
+from .models import Categories, Items, Extras
 
 # Create your views here.
 def index(request):
     context = {
-        "Categories": Categories.objects.all()
+        "Categories": Categories.objects.all(),
+        "Items": Items.objects.all()
     }
     return render(request, "orders/index.html", context)
