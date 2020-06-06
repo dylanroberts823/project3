cc3817ed6112312f408f4c0ddf87dbf73adb2002
dylanroberts823 <jguidote@hotmail.com>
2020-06-06 12:19:30 -0400

    Enabled user login and registration html pages

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+#Straight from https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
--- users/views.py
+++ users/views.py
@@ -1,6 +1,49 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from .forms import RegistrationForm
+
 
 # Create your views here.
+#Taken directly from source code 7
 def index(request):
-    return HttpResponse("Hello world!")
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "users/user.html", context)
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {"message": "Invalid credentials."})
+    else:
+        return render(request, "users/login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {"message": "Logged out."})
+
+def register_view(request):
+#Shoutout for the tutorial to https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = RegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
